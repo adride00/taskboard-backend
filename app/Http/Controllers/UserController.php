5b240e350a1a8feef72e02182a83bf251ced302a
Vladimir Diaz <128872901+LexD19@users.noreply.github.com>
2023-07-17 20:09:58 -0600

    Update UserController.php (#5)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+
+class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json($users);
     }
 
     /**
@@ -24,23 +26,25 @@ class UserController extends Controller
             'required' => 'El campo :attribute es obligatorio.',
             'string' => 'El campo :attribute debe ser una cadena de texto.',
             'max' => 'El campo :attribute no debe exceder :max caracteres.',
-        ];
+       ];
 
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string',
-            'password' => 'required|string',
-        ], $customMessages);
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6'
+        ],$customMessages);
 
-        $users = new User();
-        $users->name = $validatedData['name'];
-        $users->email = $validatedData['email'];
-        $users->password = $validatedData['password'];
-        $users->save();
+
+        $user = new User();
+        $user->name = $validatedData['name'];
+        $user->email = $validatedData['email'];
+        $user->password = bcrypt($validatedData['password']); 
+
+        $user->save();
 
         return response()->json([
             'message' => 'Nuevo usuario creado',
-            'data' => $users
+            'data' => $user
         ], 201);
     }
 
@@ -49,15 +53,36 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        return response()->json($user);
     }
+
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $customMessages = [
+            'required' => 'El campo :attribute es obligatorio.',
+            'string' => 'El campo :attribute debe ser una cadena de texto.',
+            'max' => 'El campo :attribute no debe exceder :max caracteres.',
+       ];
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email,' . $id,
+        ],$customMessages);
+
+        $user = User::findOrFail($id);
+        $user->name = $validatedData['name'];
+        $user->email = $validatedData['email'];
+        $user->save();
+
+        return response()->json([
+            'message' => 'Usuario actualizado',
+            'data' => $user
+        ]);
     }
 
     /**
@@ -65,6 +90,14 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $status = User::findOrFail($id);
+        $status->status = "inactivo";
+        $status->save();
+
+        return response()->json([
+            'message' => 'Usuario ha sido eliminado',
+            'data' => $status
+        ]);
+
     }
 }
